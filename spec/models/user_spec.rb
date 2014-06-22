0aeb99432931629a46c87bf941a56a20b6386f9b
Shinobu Okano Mac <okanoshinobu8@gmail.com>
2014-06-23 01:39:03 +0900

    Create User Model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe User do
+
+	before { @user = User.new(name:"test",email:"test@gmail.com") }
+
+	subject { @user }
+
+	it{should respond_to(:name)}
+	it{should respond_to(:email)}
+end
